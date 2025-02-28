b03d67df0a6b17aceafd9093e4205536cde08a98
himasai01 <104968925+phimasai-winfo@users.noreply.github.com>
2025-03-01 01:29:21 +0530

    feat:updated db details to fetch from .env

--- ap_cws_portal/ap_cws_portal/settings.py
+++ ap_cws_portal/ap_cws_portal/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv() 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,11 +140,11 @@ WSGI_APPLICATION = 'ap_cws_portal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.kzyuqckdydwzutgszvbh',
-        'PASSWORD': 'Anything#79894676',
-        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-        'PORT': '6543',  # Default PostgreSQL port
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
